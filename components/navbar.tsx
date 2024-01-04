0126bcd5a765d4c6f9891c02a202996c24d2a96f
sir-hector <krauskarol7@gmail.com>
2024-01-04 23:26:27 +0100

    config pretteir

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,18 @@
-import Container from "@/components/ui/container";
-import Link from "next/link";
+import Container from '@/components/ui/container'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <Container>
         <div className="relative px-2 sm:px-6 lg:px-8 flex h-16 items-center">
-        <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
-        </Link>
+          </Link>
         </div>
-    </Container>
-</div>
+      </Container>
+    </div>
+  )
+}
 
-
-  );
-};
-
-export default Navbar;
+export default Navbar
