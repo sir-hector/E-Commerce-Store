baa88702a6d9df6e2506b33d1852c50371393415
sir-hector <krauskarol7@gmail.com>
2024-01-07 22:02:16 +0100

    add categories to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,12 @@
 import Container from '@/components/ui/container'
 import MainNav from './main-nav'
 import Link from 'next/link'
+import { getCategories } from '@/actions/get-categories'
 
-const Navbar = () => {
+export const revalidate = 0
+
+const Navbar = async () => {
+  const categories = await getCategories()
   return (
     <div className="border-b">
       <Container>
@@ -10,7 +14,7 @@ const Navbar = () => {
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
           </Link>
-          <MainNav data={[]} />
+          <MainNav data={categories} />
         </div>
       </Container>
     </div>
