c94dcd1ee8feca3d0ce49a6346c937baf5660686
sir-hector <krauskarol7@gmail.com>
2024-01-24 22:45:31 +0100

    add billboards

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,20 @@
-const HomePage = () => {
-    return (
-        <div>
-            Hello Store
+import { getBillboard } from '@/actions/get-billboard'
+import Billboard from '@/components/billboard'
+import Container from '@/components/ui/container'
+
+export const revalidate = 0
+
+const HomePage = async () => {
+  const billboard = await getBillboard('fac2e03d-6e33-4867-9537-e7cacdd0e537')
+  return (
+    <div>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
         </div>
-    )
+      </Container>
+    </div>
+  )
 }
 
-export default HomePage;
+export default HomePage
---
+++ components/billboard.tsx
@@ -0,0 +1,24 @@
+import { Billboard as BillboardType } from '@/types'
+
+interface BillboardProps {
+  data: BillboardType
+}
+
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  return (
+    <div className="p-4 sm:p-6 lg:p-8 roundex-xl">
+      <div
+        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+        className="rounded-xl realtive aspect-square md:aspect-[2.4/1] overflow-hidden"
+      >
+        <div className="h-full w-full flex flex-col justify-center items-center">
+          <div className="font-bold text-3ml sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+            {/* {data.label} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Billboard
