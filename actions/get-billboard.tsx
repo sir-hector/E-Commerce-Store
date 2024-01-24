1f9d1161e027a52a9881e531fb8d61a8bf76c11b
sir-hector <krauskarol7@gmail.com>
2024-01-24 23:15:58 +0100

    fix: display billboard on main page

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -3,6 +3,7 @@ import { Billboard } from '@/types'
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`
 
 const getBillboard = async (id: string): Promise<Billboard> => {
+  console.log(`${URL}/${id}`)
   const res = await fetch(`${URL}/${id}`)
 
   return res.json()
